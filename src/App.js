9499b2184a8a299aea0e9f8ad8a9d4dc00bd9978
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2019-07-19 06:57:53 -0700

    landing page WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Spinner from './Spinner';
+import Card from '@material-ui/core/Card';
 import { ResponsiveBullet } from '@nivo/bullet';
+import Button from '@material-ui/core/Button';
+import { AppBar, CardContent, CardActions, CardHeader, CircularProgress, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 class App extends Component {
   state = {
@@ -38,29 +41,41 @@ class App extends Component {
   }
 
   render() {
-    return (
-      // <div style={{ height: '500px', width: '500px' }}>
-      //   {this.state.data ?
-      //     <ResponsiveBullet
-      //       data={this.state.data}
-      //       margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
-      //       layout='vertical'
-      //       spacing={50}
-      //       titleOffsetY={-19}
-      //       markerSize={0.9}
-      //       measureSize={0.5}
-      //       rangeColors={['rgb(98, 207, 107)', 'rgb(207, 98, 98)']}
-      //       measureColors='rgb(233, 233, 233)'
-      //       markerColors={['rgb(56, 121, 61)', 'rgb(117, 55, 55)']}
-      //     /> : <Spinner />}
-      // </div>
-      <>
-        <label>Upload a CSV File:</label>
-        <input accept='.csv' ref={(ref) => { this.uploadInput = ref; }} type='file' />
+    return (<>
+      <div style={{ display: 'flex' }}>
+        <AppBar position='static' style={{ flexDirection: 'row' }}>
+          <IconButton edge='start' color='inherit' aria-label='Menu' style={{ margin: '0 16px' }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' style={{ paddingTop: '8px' }}>Training Team DataViz</Typography>
+        </AppBar>
+      </div>
+      <div style={{ height: '500px', width: '500px' }}>
+        {this.state.data ?
+          <ResponsiveBullet
+            data={this.state.data}
+            margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
+            layout='vertical'
+            spacing={50}
+            titleOffsetY={-19}
+            markerSize={0.9}
+            measureSize={0.5}
+            rangeColors={['rgb(98, 207, 107)', 'rgb(207, 98, 98)']}
+            measureColors='rgb(233, 233, 233)'
+            markerColors={['rgb(56, 121, 61)', 'rgb(117, 55, 55)']}
+          /> : <CircularProgress />}
+      </div>
+      <Card>
+        <CardHeader title='Upload a CSV File:' />
+        <CardContent>
+          <input accept='.csv' ref={(ref) => { this.uploadInput = ref; }} type='file' />
+        </CardContent>
 
-        <button onClick={this.uploadFileHandler}>Upload</button>
-        {/* <input type='file' accept='.csv' onChange={this.uploadFileHandler}></input> */}
-      </>
+        <CardActions>
+          <Button variant="contained" color="primary" onClick={this.uploadFileHandler}>Upload</Button>
+        </CardActions>
+      </Card>
+    </>
     );
   }
 }
--- src/Spinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Spinner = (props) => (
-  <div class="loader">Loading...</div>
-);
-
-export default Spinner;
