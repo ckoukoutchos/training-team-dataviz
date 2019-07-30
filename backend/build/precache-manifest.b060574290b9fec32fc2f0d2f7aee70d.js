003c77e28aa0ed01acd0423bede24af640d4fb8a
Corbin Koukoutchos <ckoukoutchos@catalyte.io>
2019-07-30 07:42:25 -0700

    new build

---
+++ backend/build/precache-manifest.b060574290b9fec32fc2f0d2f7aee70d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287f35c8ed4c33fac50b5a5afbe6adee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d37c0678e940ea51ec",
+    "url": "/static/css/main.919d9260.chunk.css"
+  },
+  {
+    "revision": "0f8dc611d9762e0d4f5c",
+    "url": "/static/js/2.44b0a715.chunk.js"
+  },
+  {
+    "revision": "16d37c0678e940ea51ec",
+    "url": "/static/js/main.cf246e7f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
