aba250c6b7c89ca0df19730c7c125bd04c57d27b
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2019-08-06 16:58:28 -0700

    WIP

---
+++ src/containers/cycle/Cycle.tsx
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { History } from 'history';
+import MaterialTable, { MTableToolbar } from 'material-table';
+
+import Breadcrumbs from '../../components/breadcrumbs/Breadcrumbs';
+import CycleInfo from '../../components/cycle-info/CycleInfo';
+import RadarGraph from '../../components/radar-graph/RadarGraph';
+import Spinner from '../../components/spinner/Spinner';
+import Toggle from '../../components/toggle/Toggle';
+import AssociateInfo from '../../components/associate-info/AssociateInfo';
+
+import { AppState } from '../../redux/reducers/rootReducer';
+import { getUrlParams, getAssessmentTableData } from '../../shared/dataService';
+import CONSTS from '../../shared/constants';
+import styles from './Cycle.module.css';
+
+interface CycleProps {
+  history: History;
+}
+
+interface CycleState {
+  showInactive: boolean;
+  [key: string]: any;
+}
+
+class Cycle extends Component<any, CycleState> {
+  state = {
+    showInactive: false
+  };
+
+  componentDidMount() {
+    // redirect for fetch all, TODO: refactor later
+    if (this.props.cycles.length) {
+      this.props.history.push('/');
+    }
+  }
+
+  toggleHandler = () => {
+    this.setState(prevState => ({ showInactive: !prevState.showInactive }));
+  };
+
+  createTableData = (
+    cycleAggr,
+    allCycleAggr,
+    cycleMetadata,
+    cycle,
+    showInactive
+  ) => {
+    const tableData = [];
+    let leftCycle = [];
+    if (cycleMetadata[cycle]['Associate Leave']) {
+      leftCycle = cycleMetadata[cycle]['Associate Leave'].map(
+        associate => associate.name
+      );
+    }
+    Object.entries(cycleAggr[cycle]).forEach(([name, values]) => {
+      if (showInactive && leftCycle.includes(name)) {
+        tableData.push(getAssessmentTableData(name, values, allCycleAggr));
+      }
+      if (!showInactive && !leftCycle.includes(name)) {
+        tableData.push(getAssessmentTableData(name, values, allCycleAggr));
+      }
+    });
+    return tableData;
+  };
+
+  handleChange = (name: string) => (evt: any) => {
+    this.setState({ [name]: evt.target.checked });
+  };
+
+  render() {
+    const {
+      allCycleAggr,
+      cycleAggr,
+      cycleMetadata,
+      cycleMetrics,
+      history
+    } = this.props;
+    const { showInactive } = this.state;
+    const { url, cycle } = getUrlParams(history);
+
+    return !this.props.loading &&
+      cycleAggr[cycle] &&
+      cycleMetadata[cycle] &&
+      cycleMetrics[cycle] &&
+      Object.keys(allCycleAggr).length ? (
+      <div className={styles.Wrapper}>
+        <Breadcrumbs path={url} />
+
+        <CycleInfo cycleName={CONSTS[cycle]} cycle={cycleMetadata[cycle]} />
+
+        <RadarGraph
+          title='Running Averages of Assessments'
+          subtitle='Including the Max and Min Associate Running Average'
+          index='avg'
+          data={[
+            {
+              avg: 'Projects',
+              Max: cycleAggr[cycle][cycle].projectMax,
+              Min: cycleAggr[cycle][cycle].projectMin,
+              Average: cycleAggr[cycle][cycle].projectAvg
+            },
+            {
+              avg: 'Quizzes',
+              Max: cycleAggr[cycle][cycle].quizMax,
+              Min: cycleAggr[cycle][cycle].quizMin,
+              Average: cycleAggr[cycle][cycle].quizAvg
+            },
+            {
+              avg: 'Soft Skills',
+              Max: cycleAggr[cycle][cycle].softSkillsMax,
+              Min: cycleAggr[cycle][cycle].softSkillsMin,
+              Average: cycleAggr[cycle][cycle].softSkillsAvg
+            },
+            {
+              avg: 'Attempt/Pass',
+              Max: cycleAggr[cycle][cycle].attemptMax,
+              Min: cycleAggr[cycle][cycle].attemptMin,
+              Average: cycleAggr[cycle][cycle].attemptAvg
+            }
+          ]}
+          keys={['Average', 'Max', 'Min']}
+        />
+
+        <div className={styles.Paper}>
+          <MaterialTable
+            title='Associate Assessment Average & Percentile'
+            columns={[
+              { title: 'Associate', field: 'name' },
+              { title: 'Projects', field: 'projectAvg' },
+              { title: 'Quizzes', field: 'quizAvg' },
+              { title: 'Soft Skills', field: 'softSkillsAvg' },
+              { title: 'Attempt/Pass', field: 'attemptPass' }
+            ]}
+            data={this.createTableData(
+              cycleAggr,
+              allCycleAggr,
+              cycleMetadata,
+              cycle,
+              showInactive
+            )}
+            options={{
+              sorting: true,
+              pageSize: 10,
+              pageSizeOptions: [10, 20, 50]
+            }}
+            components={{
+              Toolbar: props => (
+                <>
+                  <MTableToolbar {...props} />
+                  <Toggle
+                    checked={showInactive}
+                    onChange={this.toggleHandler}
+                    leftLabel='Active'
+                    rightLabel='Inactive'
+                  />
+                </>
+              )
+            }}
+            detailPanel={[
+              {
+                tooltip: 'Show Details',
+                render: rowData => {
+                  return (
+                    <AssociateInfo
+                      bodyOnly
+                      cycle={cycle}
+                      associate={cycleMetrics[cycle].find(
+                        row => row[0].Person === rowData.name
+                      )}
+                    />
+                  );
+                }
+              }
+            ]}
+            actions={[
+              {
+                icon: 'search',
+                tooltip: 'View Associate',
+                onClick: (event, rowData) => {
+                  if (rowData.name !== cycle) {
+                    this.props.history.push(
+                      `/cycle/${cycle}/associate/${rowData.name
+                        .split(' ')
+                        .join('-')}`
+                    );
+                  }
+                }
+              }
+            ]}
+          />
+        </div>
+      </div>
+    ) : (
+      <Spinner />
+    );
+  }
+}
+
+const mapStateToProps = (state: AppState) => ({
+  allCycleAggregations: state.metrics.allCycleAggregations,
+  aggregations: state.metrics.aggregations,
+  cycles: state.metrics.cycles
+});
+
+export default connect(mapStateToProps)(Cycle);
